0f15fa4d53e0656286ffc1c8d60f24e3d04425c9
Michal Matlega <m1sz3lek@gmail.com>
2014-04-07 21:58:26 +0200

    Secure passwords

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140331162701) do
+ActiveRecord::Schema.define(version: 20140407194202) do
 
   create_table "comments", force: true do |t|
     t.string   "content"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20140331162701) do
     t.string   "phone"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
